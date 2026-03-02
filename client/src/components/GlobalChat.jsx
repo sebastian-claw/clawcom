24610044f0c16315be93b346e8608ec9337f3b05
Sebastian DeClaw <264897834+sebastian-claw@users.noreply.github.com>
2026-03-02 10:45:20 -0500

    Revert: only show typing for other users, not self

--- client/src/components/GlobalChat.jsx
+++ client/src/components/GlobalChat.jsx
@@ -21,7 +21,7 @@ function GlobalChat({ socket, comments, onClose }) {
     if (!socket) return;
 
     const handleTypingStart = ({ author: typingAuthor }) => {
-      if (true) { // Show typing for all users, including self
+      if (typingAuthor !== author) {
         // Clear any existing timeout for this user before setting a new one
         if (typingTimeoutsRef.current[typingAuthor]) {
           clearTimeout(typingTimeoutsRef.current[typingAuthor]);
